86868e6d7d34a8062d35df60354be9534caa2101
Anoop Antony <anoop@technomicssolutions.com>
2014-08-20 17:43:31 +0530

    anonymous user validation

--- resource_wagon/web/urls.py
+++ resource_wagon/web/urls.py
@@ -7,7 +7,7 @@ from web.views import Login, Logout, Home, ResetPassword, RequestView, ReplyEmpl
 urlpatterns = patterns('',
     url(r'login/$', Login.as_view(), name="login"),
     url(r'logout/$', Logout.as_view(), name="logout"),
-    url(r'^$', login_required(Home.as_view(), login_url='login'), name="home"),
+    url(r'^$', Home.as_view(), name="home"),
     url(r'^forgot_password/$', ForgotPassword.as_view(), name='forgot_password'),
     url(r'^reset_password/(?P<user_id>\d+)/$', login_required(ResetPassword.as_view()), name="reset_password"),
     url(r'^request/$', login_required(RequestView.as_view()), name="request"),
